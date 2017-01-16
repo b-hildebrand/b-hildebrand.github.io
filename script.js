89fb087aec32b5eb6e140498263b8ea286f6203f
First-Capital-Web-Design <brenthildy@Brents-MBP-2.home>
2017-01-15 20:33:41 -0500

    changed to overlay images

--- script.js
+++ script.js
@@ -1,99 +1,72 @@
 $(document).ready(function(){
 
-  var img = $('.info-button');
+//declare common selectors as variables
+var infoButton = $(".info-button"), overlay = $(".overlay-wrapper"), img = $(".item"), x = $(".x");
 
+//make info-button point at cursor ;-)
     var center_x;
     var center_y;
 
-
-    var offset = img.offset();
-    function mouse(evt){
-        center_x = (offset.left) + (img.width()/2);
-        center_y = (offset.top) + (img.height()/2);
+    var offset = infoButton.offset();
+function mouse(evt){
+        center_x = (offset.left) + (infoButton.width()/2);
+        center_y = (offset.top) + (infoButton.height()/2);
         var mouse_x = evt.pageX; var mouse_y = evt.pageY;
         var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
         var degree = (radians * (180 / Math.PI) * -1) + 0;
-        img.css('-moz-transform', 'rotate('+degree+'deg)', '2s');
-        img.css('-webkit-transform', 'rotate('+degree+'deg)', '2s');
-        img.css('-o-transform', 'rotate('+degree+'deg)');
-        img.css('-ms-transform', 'rotate('+degree+'deg)');
-    }
+        infoButton.css('-moz-transform', 'rotate('+degree+'deg)', '2s');
+        infoButton.css('-webkit-transform', 'rotate('+degree+'deg)', '2s');
+        infoButton.css('-o-transform', 'rotate('+degree+'deg)');
+        infoButton.css('-ms-transform', 'rotate('+degree+'deg)');
+}
+
     $(document).mousemove(mouse);
 
+//view info
+$(".info-button").click(function(){
 
+      $(".info-button").toggleClass("info-button-active");
+      $(".main-wrapper").toggleClass("main-wrapper-info-active");
+      $(".main-wrapper").toggleClass("no-scroll");
+      $(".info-wrapper").toggleClass("info-wrapper-active");
+      $(".resume-wrapper").toggleClass("resume-wrapper-info-active");
 
-
-
-    $(".info-button").click(function(){
-
-        $(".info-button").toggleClass("info-button-active");
-        $(".main-wrapper").toggleClass("main-wrapper-info-active");
-        $(".main-wrapper").toggleClass("no-scroll");
-        $(".info-wrapper").toggleClass("info-wrapper-active");
-        $(".resume-wrapper").toggleClass("resume-wrapper-info-active");
-
-        $(".main-wrapper").removeClass("main-wrapper-resume-active");
-        $(".info-wrapper").removeClass("info-wrapper-resume-active");
-        $(".resume-wrapper").removeClass("resume-wrapper-active");
-
-
-            // center_x = (offset.left) + (img.width()/2) + 400;
-            // center_y = (offset.top) + (img.height()/2);
-
-    });
-
-    $(".resume").click(function(){
-
-        $(".resume-wrapper").toggleClass("resume-wrapper-active");
-        $(".info-wrapper").toggleClass("info-wrapper-resume-active");
-        $(".main-wrapper").toggleClass("main-wrapper-resume-active");
-
-    });
-
-
-
-
-    //image zoom
-
-    $(".item-test").click(function(){
-
-      $(".section-first").removeClass("section-first-active");
-
-      $(".item-test > img").height("225");
-      $(this).children("img").height("400");
-
-      if(($(this).parent().children(".vertical-rule-active").length > 0)){
-        console.log("test1");
-
-      } else {
-
-        console.log("test");
-
-        $(".vertical-rule").removeClass("vertical-rule-active");
-
-        $(this).parent().children(".vertical-rule").addClass("vertical-rule-active");
-      }
-
-    });
-  //
-
-//changes top-margin for first section when active
-
-    $(".section-first").click(function(){
-      $(".section-first").addClass("section-first-active");
-    })
-
-//smooth scroll
-
-$('.section').click( function() {
-
-  //console.log($(this).position().top);
-
-    //$(".main-wrapper").animate({ scrollTop: $(this).offset().top}, 200);
+      $(".main-wrapper").removeClass("main-wrapper-resume-active");
+      $(".info-wrapper").removeClass("info-wrapper-resume-active");
+      $(".resume-wrapper").removeClass("resume-wrapper-active");
 
 });
 
+//view resume
+$(".resume").click(function(){
+
+      $(".resume-wrapper").toggleClass("resume-wrapper-active");
+      $(".info-wrapper").toggleClass("info-wrapper-resume-active");
+      $(".main-wrapper").toggleClass("main-wrapper-resume-active");
+
+});
+
+//image zoom overlay
+img.click(function(){
+
+      var backImg = $(this).children().prop('src');
+      var cap = $(this).children('.hidden-cap').html();
+      $('.zoom').css('background-image' , 'url(' + backImg + ')');
+      $('.shown-cap').html('');
+      $('.shown-cap').html(cap);
+      overlay.fadeIn(400);
+      $(".info-button").toggleClass("info-button-active");
+
+});
+
+x.click(function(){
+      overlay.fadeOut(400);
+      $(".info-button").toggleClass("info-button-active");
+});
+
+//form submit
 var $form = $('form');
+
 $form.submit(function(){
    $.post($(this).attr('action'), $(this).serialize(), function(response){
          $("form").html("");
@@ -101,8 +74,5 @@ $form.submit(function(){
    },'json');
    return false;
 });
-
-
-
 
 });
